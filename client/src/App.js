290d70a9de8c23688fec1aedba826005dcef33d9
krushnabiradar <krushnabiradar1995@.com>
2024-03-14 20:15:18 +0530

    updated login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginForm from './LoginForm';
-import RegisterForm from './RegisterForm';
-import StaticTable from './StaticTable';
-import NavigationBar from './NavigationBar';
+// App.js
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
+import StaticTable from "./StaticTable";
+import NavigationBar from "./NavigationBar";
 
 const App = () => {
+  const isAuthenticated = localStorage.getItem("user") !== null;
+
   return (
     <Router>
       <NavigationBar />
       <Routes>
+        {/* <Route path="/" element={<Navigate to="/login" />} /> */}
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
-        <Route path="/table" element={<StaticTable />} />
-        {/* Add more routes as needed */}
+        <Route
+          path="/table"
+          element={isAuthenticated ? <StaticTable /> : <Navigate to="/login" />}
+        />
       </Routes>
     </Router>
   );
